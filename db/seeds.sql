9d8791ac79993d0ad6b9b1db93403541ab2b1f4f
John Fields <jjfields24@outlook.com>
2020-02-22 23:09:55 -0500

    working

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+insert into burgers
+
+    (burger_name, devoured)
+
+values
+
+    ('Cheese', false),
+
+    ('Double', false),
+
+    ('Pizza', false),
+
+    ('Jalapeno', false),
+
+    ('Swiss', false);
